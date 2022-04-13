e18f240a245913d0aa4e0862c633519e3526f47d
Amina <m.djarfi.amina@gmail.com>
2022-04-13 11:30:47 +0200

    ajout script1

---
+++ TP1/script1.py
@@ -0,0 +1,22 @@
+
+import sys 
+
+
+args = sys.argv[1:]
+
+
+filein = args[0]
+print(filein)
+fileout = args[1]
+print(fileout)
+with open(fileout, 'w') as txtfile:
+    
+    with open(filein, 'r', newline='') as csvfile:
+        
+        for line in csvfile:
+            row = line.split()    
+            txtfile.write('_'.join(row))
+            txtfile.write(' ')
+            if row[0] == '.':
+                txtfile.write('\n')
+
